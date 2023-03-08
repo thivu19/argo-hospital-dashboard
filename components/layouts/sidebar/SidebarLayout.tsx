4b1ada6f9174c12f1a624061be06b9390312d1ec
nwe3er <nebilweber.nw@gmail.com>
2023-03-07 23:22:59 -0600

    feat: created SidebarLayout

---
+++ components/layouts/sidebar/SidebarLayout.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import styles from './SidebarLayout.module.css';
+
+export interface ISidebarLayout {}
+
+const SidebarLayout: React.FC<ISidebarLayout> = () => {
+  return (
+    <nav className={styles.nav}>
+      <input className={styles.input} placeholder="Search..." />
+      <Link href="/">Home</Link>
+      <Link href="/">About</Link>
+    </nav>
+  );
+};
+
+export default SidebarLayout;
