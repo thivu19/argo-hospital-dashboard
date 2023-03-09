da5a27f73dc915ff15713b3f7d81767e7eea61de
nwe3er <nebilweber.nw@gmail.com>
2023-03-09 16:36:39 -0600

    test: tested adding a component in js

---
+++ components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Button.css';
+
+const Button = ({ children, ...rest }) => {
+    return (
+        <button className="button red" {...rest}>
+            { children }
+        </button>
+    )
+}
+
+export default Button;
