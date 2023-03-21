49ab5dddb07636239fafebab55936bf2f2453f5a
sba190007 <102188621+sba190007@users.noreply.github.com>
2023-03-21 18:58:29 -0500

    test: test adding component

---
+++ components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Card.css';
+
+const Card = ({ title, subheading, image}) => {
+    return (
+        <div className="card">
+            <h2 className="card-title">{ title }</h2>
+            <img src={ image }/>
+            <p className="card-subheading">{ subheading }</p>
+        </div>
+    )
+}
+
+export default Card;
