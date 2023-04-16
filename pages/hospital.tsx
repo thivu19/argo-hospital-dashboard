d1b50332eb0b956f0612e0310b1488e12d34b5d3
Cam Chu <camphuongchu@gmail.com>
2023-04-15 23:51:29 -0500

    feat: added new side bar

--- pages/hospital.tsx
+++ pages/hospital.tsx
@@ -2,11 +2,12 @@ import options from '@/components/HighCharts/barChart';
 import lengthOfStay from '@/components/HighCharts/containerChart';
 import mortalityRate from '@/components/HighCharts/lineChart';
 import cost from '@/components/HighCharts/radiusPieChart';
-import { AppBar, Box, Typography } from '@mui/material';
+import Button from '@material-ui/core/Button';
+import { Box, Typography } from '@mui/material';
+import { action } from '@storybook/addon-actions';
 import * as Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { useRef } from 'react';
-import Header from '../components/Header/Header';
 import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
 import styles from '../styles/Hospital.module.css';
@@ -54,6 +55,15 @@ const Hospital: NextPageWithLayout = (props: HighchartsReact.Props) => {
             {...props}
           />
         </Box>
+        <Box>
+          <Button
+            color="primary"
+            onClick={action('Secondary button clicked')}
+            variant="contained"
+          >
+            Secondary
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
@@ -63,24 +73,9 @@ export default Hospital;
 
 Hospital.getLayout = (page) => {
   return (
-    <Box>
-      <Box>
-        <AppBar>
-          <Typography>
-            <Header>BHCareChain</Header>
-          </Typography>
-        </AppBar>
-        <br></br>
-        <br></br>
-        <br></br>
-      </Box>
-
-      <Box>
-        <PrimaryLayout>
-          <SidebarLayout />
-          {page}
-        </PrimaryLayout>
-      </Box>
-    </Box>
+    <PrimaryLayout>
+      <SidebarLayout />
+      {page}
+    </PrimaryLayout>
   );
 };
